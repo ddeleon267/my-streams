32b24af7e68be75d0eb7d5966afe35bed82e4f2a
github username <github email address>
2019-11-08 08:33:06 -0500

    Initiate Google API client.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  componentDidMount(){
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '727962166634-e32mlaj9opmv4nt9nnacs9uulgs0cl7f.apps.googleusercontent.com',
+        scope: 'email'
+      })
+    })
+  }
+
   render() {
     return <div>Google Auth</div>
   }
