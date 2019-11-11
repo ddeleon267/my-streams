7713b79ac68e8979a96057c2078d457ac9f4ea4d
github username <github email address>
2019-11-11 09:33:45 -0500

    Set up gapi log-in.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = {
+    isSignedIn: null
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>
+    } else if (this.state.isSignedIn){
+      return <div>I am signed in</div>
+    } else {
+      return <div>I am not signed in</div>
+    }
+  }
   componentDidMount(){
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '727962166634-e32mlaj9opmv4nt9nnacs9uulgs0cl7f.apps.googleusercontent.com',
-        scope: 'email'
+        scope: 'email '
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get()})
       })
     })
   }
 
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 };
 
